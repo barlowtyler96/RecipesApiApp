7858489f3fd09ce644b8dd88b288bd0eb07db6d8
Tyler Barlow <barlowtyler96@gmail.com>
2023-06-09 15:22:15 -0400

    Add project files.

---
+++ DataAccess/Models/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models;
+
+internal class Categories
+{
+    //list of occasions
+    //list of meal types
+
+}
---
+++ DataAccess/Models/Ingredients.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models;
+
+internal class Ingredients
+{
+    //list of meats
+    //list of spices
+    //list ofvegetables
+    //list of fruits
+}
