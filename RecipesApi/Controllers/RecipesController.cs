49942eae03c60abe5b64e6db5b2104eed88f7308
Tyler Barlow <barlowtyler96@gmail.com>
2023-06-12 16:58:10 -0400

    Created Recipe Controller

---
+++ RecipesApi/Controllers/RecipesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RecipesApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RecipesController : ControllerBase
+{
+    // GET: api/Recipes
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/Recipes
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/Recipes
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/Recipes/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/Recipes/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
