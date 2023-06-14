cbb049afea8c68bfd9788f29411bce03c640d764
Tyler Barlow <barlowtyler96@gmail.com>
2023-06-14 16:42:12 -0400

    added LoadData method using dapper

--- RecipeLibrary/DataAccess/SqlDataAccess.cs
+++ RecipeLibrary/DataAccess/SqlDataAccess.cs
@@ -1,6 +1,47 @@
-﻿namespace RecipeLibrary.DataAccess;
+﻿using Microsoft.Extensions.Configuration;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace RecipeLibrary.DataAccess;
 
 public class SqlDataAccess
 {
+    private readonly IConfiguration _config;
 
+    public SqlDataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    
+
+    public async Task<List<T>> LoadData<T, U>(
+        string storedProcedure,
+        U parameters,
+        string connectionStringName)
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName);
+
+        using IDbConnection connection = new SqlConnection(connectionString);
+
+        var rows = await connection.QueryAsync<T>(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure);
+        return rows.ToList();
+    }
+
+    // ABOVE THIS METHOD IS THE ASYNC VERSION
+    //public List<T> LoadData<T, U>(
+    //    string storedProcedure,
+    //    U parameters,
+    //    string connectionStringName)
+    //{
+    //    string connectionString = _config.GetConnectionString(connectionStringName);
+
+    //    using IDbConnection connection = new SqlConnection(connectionString);
+
+    //    List<T> rows = connection.Query<T>(storedProcedure, parameters,
+    //        commandType: CommandType.StoredProcedure).ToList();
+    //    return rows;
+    //}
 }
