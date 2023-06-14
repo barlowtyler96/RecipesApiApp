95d3db7edd01ea145d83eb7d51f9c4b1ecb862fc
Tyler Barlow <barlowtyler96@gmail.com>
2023-06-14 17:39:32 -0400

    added RecipeData class

---
+++ RecipeLibrary/DataAccess/RecipeData.cs
@@ -0,0 +1,29 @@
+﻿using RecipeLibrary.Models;
+
+namespace RecipeLibrary.DataAccess;
+
+public class RecipeData
+{
+    private readonly ISqlDataAccess _sql;
+
+    public RecipeData(ISqlDataAccess sql)
+    {
+        _sql = sql;
+    }
+
+    public Task<List<RecipeModel>> GetNameDescription()
+    {
+        return _sql.LoadData<RecipeModel, dynamic>(
+            "dbo.spRecipes_GetNameDescription",
+            new { },
+            "Default");
+    }
+    
+    public Task<List<RecipeModel>> GetByName(string name)
+    {
+        return _sql.LoadData<RecipeModel, dynamic>(
+            "dbo.spRecipes_GetByName",
+            new { Name = name},
+            "Default");
+    }
+}
