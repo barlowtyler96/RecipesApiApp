faace4be69b1b222da7c455dac9e0c938a673d56
Tyler Barlow <barlowtyler96@gmail.com>
2023-06-15 18:55:45 -0400

    DB design fail. Redesigning

--- RecipeLibrary/Models/IngredientQuantity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RecipeLibrary.Models;
-
-internal class IngredientQuantity
-{
-    public int Id { get; set; } 
-
-    public string? Quantity { get; set; }
-}
--- RecipeLibrary/Models/IngredientUnit.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RecipeLibrary.Models;
-
-internal class IngredientUnit
-{
-    public int Id { get; set; }
-
-    public string? Unit { get; set; }
-
-}
