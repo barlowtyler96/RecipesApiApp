63439ca01de0f5397305e974ebe1a53453e2ff78
Tyler Barlow <barlowtyler96@gmail.com>
2023-06-15 20:30:53 -0400

    abstracted services from program.cs into new class

--- RecipesApi/Program.cs
+++ RecipesApi/Program.cs
@@ -2,40 +2,13 @@ using Microsoft.IdentityModel.Tokens;
 using RecipesApi.Constants;
 using System.Text;
 using RecipeLibrary.DataAccess;
-
+using RecipesApi.StartupConfig;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
-builder.Services.AddSingleton<IRecipeData, RecipeData>();
-builder.Services.AddAuthorization(opts =>
-{
-    opts.AddPolicy(PolicyConstants.MustBeAnAdmin, policy =>
-    {
-        policy.RequireClaim("true");
-    });
-});
-
-builder.Services.AddHealthChecks()
-    .AddSqlServer(builder.Configuration.GetConnectionString("Default"));
-
-builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer(opts =>
-    {
-        opts.TokenValidationParameters = new()
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
-            ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.ASCII.GetBytes(
-                builder.Configuration.GetValue<string>("Authentication:SecretKey")))
-        };
-    });
+builder.AddStandardServices();
+builder.AddAuthServices();
+builder.AddHealthCheckServices();
+builder.AddCustomServices();
 
 var app = builder.Build();
 
---
+++ RecipesApi/StartupConfig/DependencyInjectionExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using RecipeLibrary.DataAccess;
+using RecipesApi.Constants;
+using System.Text;
+
+namespace RecipesApi.StartupConfig;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddStandardServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        
+        
+    }
+
+    public static void AddCustomServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+        builder.Services.AddSingleton<IRecipeData, RecipeData>();
+    }
+
+    public static void AddHealthCheckServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddHealthChecks()
+            .AddSqlServer(builder.Configuration.GetConnectionString("Default"));
+    }
+
+    public static void AddAuthServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthorization(opts =>
+        {
+            opts.AddPolicy(PolicyConstants.MustBeAnAdmin, policy =>
+            {
+                policy.RequireClaim("true");
+            });
+        });
+
+        builder.Services.AddAuthentication("Bearer")
+            .AddJwtBearer(opts =>
+            {
+                opts.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration.GetValue<string>("Authentication:Issuer"),
+                    ValidAudience = builder.Configuration.GetValue<string>("Authentication:Audience"),
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.ASCII.GetBytes(
+                        builder.Configuration.GetValue<string>("Authentication:SecretKey")))
+                };
+            });
+    }
+}
