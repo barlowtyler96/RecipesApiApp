ea7394a6c092d27056f439ff934d0729c7a9a616
Tyler Barlow <barlowtyler96@gmail.com>
2023-06-20 10:28:06 -0400

    initial cleanup of blazor app

--- RecipesBlazorUI/Program.cs
+++ RecipesBlazorUI/Program.cs
@@ -3,13 +3,11 @@ using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Error");
