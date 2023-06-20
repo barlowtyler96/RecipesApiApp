1b8e93932188baeb4cfe453d82804748e9d64cbd
Tyler Barlow <barlowtyler96@gmail.com>
2023-06-20 15:01:15 -0400

    client is connected and working. need to add more html/css to this allrecipes page

---
+++ RecipesBlazorUI/Models/RecipeModel.cs
@@ -0,0 +1,14 @@
+﻿namespace RecipesBlazorUI.Models;
+
+public class RecipeModel
+{
+	public int Id { get; set; }
+
+	public string? Name { get; set; }
+
+	public string? Description { get; set; }
+
+	public string? Ingredients { get; set; }
+
+	public string? Instructions { get; set; }
+}
