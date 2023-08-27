a04d9b445c7cb2ba0caff57d61dc5079b921e38d
Tyler Barlow <barlowtyler96@gmail.com>
2023-08-27 19:22:53 -0400

    fixed model entity and dto

--- RecipeLibrary/Models/RecipeDto.cs
+++ RecipeLibrary/Models/RecipeDto.cs
@@ -1,18 +1,16 @@
-﻿
-namespace RecipeLibrary.Models;
+﻿namespace RecipeLibrary.Models;
 
 public class RecipeDto
 {
-    public int Id { get; set; }
+    public int RecipeId { get; set; }
 
-    public string? Name { get; set; }
+    public string? Name { get; set; }    
 
     public string? Description { get; set; }
 
     public string? Instructions { get; set; }
 
+    public DateTime DateAdded { get; set; } = DateTime.UtcNow;
+
     public string? ImagePath { get; set; }
-
-    public List<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
-
 }
--- RecipeLibrary/Models/RecipeModel.cs
+++ RecipeLibrary/Models/RecipeModel.cs
@@ -1,16 +1,18 @@
-﻿namespace RecipeLibrary.Models;
+﻿
+namespace RecipeLibrary.Models;
 
-public class RecipeModel
+public class RecipeModel    
 {
     public int RecipeId { get; set; }
 
-    public string? Name { get; set; }    
+    public string? Name { get; set; }
 
     public string? Description { get; set; }
 
     public string? Instructions { get; set; }
 
-    public DateTime DateAdded { get; set; } = DateTime.UtcNow;
-
     public string? ImagePath { get; set; }
+
+    public List<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
+
 }
