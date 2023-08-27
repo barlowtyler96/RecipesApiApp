6d1000274abfd6833ebd65d152c21b9d0ac7b92d
Tyler Barlow <barlowtyler96@gmail.com>
2023-08-27 19:21:34 -0400

    type changes

--- RecipeLibrary/DataAccess/IRecipeData.cs
+++ RecipeLibrary/DataAccess/IRecipeData.cs
@@ -5,11 +5,11 @@ namespace RecipeLibrary.DataAccess
     public interface IRecipeData
     {
         Task Delete(int recipesId);
-        Task<List<RecipeModel>> GetAll();
-        Task UpdateAllColumns(int recipeId, RecipeModel recipeModel);
+        Task<List<RecipeDto>> GetAll();
+        Task UpdateAllColumns(int recipeId, RecipeDto recipeDto);
+        Task<List<RecipeModel>> GetByDate();
+        Task<List<RecipeModel>> GetById(int id);
+        Task<int> Create(RecipeModel recipeModel);
         Task<PaginationResponse<List<RecipeModel>>> GetByKeyword(string keyword, int currentPageNumber, int pageSize);
-        Task<List<RecipeDto>> GetByDate();
-        Task<List<RecipeDto>> GetById(int id);
-        Task<int> Create(RecipeDto recipeDto);
     }
 }
--- RecipeLibrary/DataAccess/ISqlDataAccess.cs
+++ RecipeLibrary/DataAccess/ISqlDataAccess.cs
@@ -5,9 +5,7 @@ namespace RecipeLibrary.DataAccess
     public interface ISqlDataAccess
     {
         Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName);
-        Task<PaginationResponse<List<RecipeModel>>> LoadMultiData<T, U>(string storedProcedure, U parameters, 
-                                                                        string connectionStringName,
-                                                                        int currentPageNumber, int pageSize);
+        Task<PaginationResponse<List<RecipeModel>>> LoadMultiData<T, U>(string storedProcedure, U parameters, string connectionStringName, int currentPageNumber, int pageSize);
         Task SaveData<T>(string storedProcedure, T parameters, string connectionStringName);
     }
 }
