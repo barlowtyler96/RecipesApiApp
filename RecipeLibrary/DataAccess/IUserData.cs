0c074de2640c42caa36d0c7567a6ca98561d0265
Tyler Barlow <barlowtyler96@gmail.com>
2023-08-31 19:45:39 -0400

    added service for user favorites

---
+++ RecipeLibrary/DataAccess/IUserData.cs
@@ -0,0 +1,9 @@
+﻿using RecipeLibrary.Models;
+namespace RecipeLibrary.DataAccess;
+
+public interface IUserData
+{
+    Task AddUserFavorite(UserFavorite userFavorite);
+    Task DeleteUserFavorite(UserFavorite userFavorite);
+    Task<List<UserFavorite>> GetUserFavoritesIds(string userSub);
+}
---
+++ RecipeLibrary/DataAccess/UserData.cs
@@ -0,0 +1,48 @@
+﻿using RecipeLibrary.Models;
+namespace RecipeLibrary.DataAccess;
+
+public class UserData : IUserData
+{
+    private readonly ISqlDataAccess _sql;
+
+    public UserData(ISqlDataAccess sql)
+    {
+        _sql = sql;
+    }
+
+
+    public async Task AddUserFavorite(UserFavorite userFavorite)
+    {
+        await _sql.SaveData<dynamic>(
+            "spAddUserFavorite",
+            new
+            {
+                UserSub = userFavorite.UserSub,
+                RecipeId = userFavorite.RecipeId
+            },
+            "Default");
+    }
+
+    public async Task DeleteUserFavorite(UserFavorite userFavorite) 
+    {
+        await _sql.SaveData<dynamic>(
+            "spDeleteUserFavorite",
+            new
+            {
+                UserSub = userFavorite.UserSub,
+                RecipeId = userFavorite.RecipeId
+            },
+            "Default");
+    }
+
+    public async Task<List<UserFavorite>> GetUserFavoritesIds(string userSub)
+    {
+        return await _sql.LoadData<UserFavorite, dynamic>(
+            "spGetUserFavoriteIds",
+            new
+            {
+                UserSub = userSub
+            },
+            "Default");
+    }
+}
