a5c8127ae005dcb2b591160fb611af580cb78d83
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-02 21:13:34 -0400

    made public

--- RecipeLibrary/Models/IngredientModel.cs
+++ RecipeLibrary/Models/IngredientModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace RecipeLibrary.Models;
 
-internal class IngredientModel
+public class IngredientModel
 {
     public int IngredientId { get; set; } 
 
