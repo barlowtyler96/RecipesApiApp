30e7fe78572cc6fcac6db3535889fd174ba193f6
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-03 17:37:32 -0400

    accessing sub to store who created the recipe

--- RecipesApi/Controllers/AdministratorController.cs
+++ RecipesApi/Controllers/AdministratorController.cs
@@ -12,20 +12,26 @@ namespace RecipesApi.Controllers
 	{
 		private readonly IRecipeData _data;
 		private readonly ILogger _logger;
-		public AdministratorController(IRecipeData data, ILogger<AdministratorController> logger)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public AdministratorController(IRecipeData data, ILogger<AdministratorController> logger, 
+			IHttpContextAccessor httpContextAccessor)
 		{
 			_data = data;
 			_logger = logger;
-		}
+            _httpContextAccessor = httpContextAccessor;
+        }
 
 		// POST api/Administrator
 		[HttpPost]
-		public async Task<ActionResult> Post([FromBody] RecipeModel recipeDto)
+		public async Task<ActionResult> Post([FromBody] RecipeModel recipeModel)
 		{
-			_logger.LogInformation("POST: api/Administrator");
+			recipeModel.CreatedBy = _httpContextAccessor.HttpContext!.User.Claims.FirstOrDefault(x => x.Type == "sub")?.Value;
+
+            _logger.LogInformation("POST: api/Administrator");
 			try
 			{
-				var output = await _data.Create(recipeDto);
+				var output = await _data.Create(recipeModel);
 				return Ok(output);
 			}
 			catch (Exception ex)
@@ -34,7 +40,7 @@ namespace RecipesApi.Controllers
 					ex,
 					"The POST call to api/Administrator failed. Recipe model was " +
 					"Name: {Name} Description: {Description} Instructions: {Instructions}",
-					recipeDto.Name, recipeDto.Description, recipeDto.Instructions);
+					recipeModel.Name, recipeModel.Description, recipeModel.Instructions);
 				return BadRequest();
 			}
 		}
