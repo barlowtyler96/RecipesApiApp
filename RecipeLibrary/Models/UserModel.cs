131e0881952687d544fa0ad2ac50d92c4a1440df
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-09 19:41:29 -0400

    added user model

---
+++ RecipeLibrary/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace RecipeLibrary.Models;
+
+public class UserModel
+{
+    public int UserId { get; set; }
+
+    public string? UserSub { get; set; }
+
+    public string? FirstName { get; set; }   
+
+    public string? LastName { get; set; }
+}
