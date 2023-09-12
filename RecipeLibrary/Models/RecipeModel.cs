64f31717e0f4cecf7dc1efe01a4aa3499d91e9fd
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-12 16:12:33 -0400

    added constructor

--- RecipeLibrary/Models/RecipeModel.cs
+++ RecipeLibrary/Models/RecipeModel.cs
@@ -16,4 +16,14 @@ public class RecipeModel
 
     public List<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
 
+    public RecipeModel(RecipeDto recipesDto, List<RecipeIngredient> recipeIngredients)
+    {
+        RecipeId = recipesDto.RecipeId;
+        Name = recipesDto.Name;
+        Description = recipesDto.Description;
+        Instructions = recipesDto.Instructions;
+        CreatedBy = recipesDto.CreatedBy;
+        ImageUrl = recipesDto.ImageUrl;
+        RecipeIngredients = recipeIngredients.Where(ri => ri.RecipeId == recipesDto.RecipeId).ToList();
+    }
 }
