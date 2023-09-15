45f6f69660295da451d6b9ba969c1d17a216341f
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-15 13:17:22 -0400

    allowing swagger in prod

--- RecipesApi/Program.cs
+++ RecipesApi/Program.cs
@@ -10,11 +10,8 @@ builder.AddCustomServices();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
