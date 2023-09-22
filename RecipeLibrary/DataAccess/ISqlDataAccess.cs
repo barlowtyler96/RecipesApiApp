ec9823bf81d9fb69522d1c12078e9c23ccbfd13b
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-21 21:56:42 -0400

    make naming better and add LoadRecipeModelData

--- RecipeLibrary/DataAccess/ISqlDataAccess.cs
+++ RecipeLibrary/DataAccess/ISqlDataAccess.cs
@@ -5,7 +5,19 @@ namespace RecipeLibrary.DataAccess
     public interface ISqlDataAccess
     {
         Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName);
-        Task<PaginationResponse<List<RecipeDto>>> LoadMultiData<T, U>(string storedProcedure, U parameters, string connectionStringName, int currentPageNumber, int pageSize);
+
+        Task<RecipeModel> LoadRecipeModelData<T, U>(
+            string storedProcedure,
+            U parameters,
+            string connectionStringName);
+
+        Task<PaginationResponse<List<RecipeDto>>> LoadPaginationData<T, U>(
+            string storedProcedure, 
+            U parameters, 
+            string connectionStringName, 
+            int currentPageNumber, 
+            int pageSize);
+
         Task SaveData<T>(string storedProcedure, T parameters, string connectionStringName);
     }
 }
