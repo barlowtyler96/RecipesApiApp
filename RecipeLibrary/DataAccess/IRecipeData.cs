875c34e1467bbe41b5f8290c089dd578afa5bbc6
Tyler Barlow <barlowtyler96@gmail.com>
2023-11-22 19:33:54 -0500

    change to file scope namespace

--- RecipeLibrary/DataAccess/IRecipeData.cs
+++ RecipeLibrary/DataAccess/IRecipeData.cs
@@ -1,13 +1,12 @@
 ﻿using RecipeLibrary.Models;
 
-namespace RecipeLibrary.DataAccess
+namespace RecipeLibrary.DataAccess;
+
+public interface IRecipeData
 {
-    public interface IRecipeData
-    {
-        Task<PaginationResponse<List<RecipeDto>>> GetAllRecipeDtos(int currentPageNumber, int pageSize);
-        Task<PaginationResponse<List<RecipeModel>>> GetAllRecipeModels(int currentPageNumber, int pageSize);
-        Task<PaginationResponse<List<RecipeDto>>> GetByDate(int currentPageNumber, int pageSize);
-        Task<RecipeModel> GetById(int id);
-        Task<PaginationResponse<List<RecipeDto>>> GetByKeyword(string keyword, int currentPageNumber, int pageSize);
-    }
+    Task<PaginationResponse<List<RecipeDto>>> GetAllRecipeDtos(int currentPageNumber, int pageSize);
+    Task<PaginationResponse<List<RecipeModel>>> GetAllRecipeModels(int currentPageNumber, int pageSize);
+    Task<PaginationResponse<List<RecipeDto>>> GetByDate(int currentPageNumber, int pageSize);
+    Task<RecipeModel> GetById(int id);
+    Task<PaginationResponse<List<RecipeDto>>> GetByKeyword(string keyword, int currentPageNumber, int pageSize);
 }
